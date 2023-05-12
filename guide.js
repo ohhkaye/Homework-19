8eee42c79df0206ad67edde889aeb5227ec3e919
ohhkaye <hetrick.haley@gmail.com>
2023-05-11 20:21:58 -0400

    Homework 19 finalization

--- guide.js
+++ guide.js
@@ -1,2 +1,15 @@
 
 console.log("Hello world");
+
+$(document).ready(function(){
+    $('.top').hide();
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 200) {
+          //  perform this if true
+          $('.top').fadeIn();
+        } else {
+            // perform this if not true (or false)
+            $('.top').fadeOut();
+        }
+    });
+});
